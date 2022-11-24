4f2cf5964dcbab878d5ae1785a850acc93981c9c
Abraham <abrahammorillo777@gmail.com>
2022-11-23 20:58:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState([
+    {"id": 1, "title": "first task", "state": false},
+    {"id": 2, "title": "second task", "state": false},
+  ])
+
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask) {
+      let id = toDoList.length + 1;
+      let newRecord = {id: id, title: newTask, state: false};
+      setToDoList([...toDoList, newRecord]);
+      setNewTask('');
+    }
+  }
+
+  const deleteTask = (id) => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container App">
+      <h2>To Do List App</h2>
+      {toDoList.length == 0 ? 'No task' : ''}
+
+      <div className='row'>
+        <div className='col'>
+          <input value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder='Add new task'></input>
+          <button onClick={addTask} className='btn btn-success'>Add task</button>
+        </div>
+      </div>
+
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Key</th>
+            <th scope="col">Task Name</th>
+            <th scope="col">Status</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            {toDoList.map((task, id) => {
+              return(
+                <React.Fragment key={task.id}>
+                  <tr>
+                    <th scope="row">{task.id}</th>
+                    <td>{task.title}</td>
+                    <td>{task.state == false ? 'Not Completed' : 'Complete'}</td>
+                    <td>
+                      <button className='btn btn-danger'>Edit</button>
+                      <button className='btn btn-primary'>Delete</button>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              )
+            })}
+        </tbody>
+      </table>
+      </div>
+    );
 }
 
 export default App;
